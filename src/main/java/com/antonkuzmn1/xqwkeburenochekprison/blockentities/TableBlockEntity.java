42c90b6c430f1e0ecfc622bab6e0aeffc5324509
anton <antonkuzmn1@gmail.com>
2026-01-11 23:23:50 +0700

    global overriding getRenderBoundingBox

--- src/main/java/com/antonkuzmn1/xqwkeburenochekprison/blockentities/TableBlockEntity.java
+++ src/main/java/com/antonkuzmn1/xqwkeburenochekprison/blockentities/TableBlockEntity.java
@@ -4,6 +4,7 @@ import com.antonkuzmn1.xqwkeburenochekprison.registry.ModBlockEntities;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import software.bernie.geckolib.animatable.GeoBlockEntity;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
@@ -24,5 +25,10 @@ public class TableBlockEntity extends BlockEntity implements GeoBlockEntity {
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return cache;
+    }
+
+    @Override
+    public AABB getRenderBoundingBox() {
+        return new AABB(worldPosition).inflate(3.0);
     }
 }
